6c6dcde4060b34982836568d96424d26427983fb
Jerry <55096594+jerryf276@users.noreply.github.com>
2025-11-06 00:50:18 +0000

    added: camera

--- honours-proj-feasibility-demo/Player.cs
+++ honours-proj-feasibility-demo/Player.cs
@@ -10,8 +10,10 @@ public partial class Player : RigidBody3D
 
     public override void _Process(double delta)
     {
-        var input = Input.GetActionStrength("move_forward");
-        ApplyCentralForce(new Vector3(Vector3.Forward.X, Vector3.Forward.Y, Vector3.Forward.Z * input * 1200.0f * (float)delta));
-        GD.Print(Position);
+        Vector3 input = Vector3.Zero;
+        input.X = Input.GetAxis("move_left", "move_right");
+        input.Z = Input.GetAxis("move_forward", "move_backward");
+
+        ApplyCentralForce(input * 1200.0f * (float)delta);
     }
 }
