2ae5870d548a5d3281b2654ec7f02ee787f521a3
Jerry <55096594+jerryf276@users.noreply.github.com>
2025-11-06 14:10:24 +0000

    commit: camera now moves with the mouse

--- honours-proj-feasibility-demo/Player.cs
+++ honours-proj-feasibility-demo/Player.cs
@@ -4,40 +4,53 @@ using System;
 public partial class Player : RigidBody3D
 {
 
-    float mouseSensitivity = 0.001f;
-    float twistInput = 0.0f;
-    float pitchInput = 0.0f;
-    float playerSpeed = 1200.0f;
+	float mouseSensitivity = 0.001f;
+	float twistInput = 0.0f;
+	float pitchInput = 0.0f;
+	float playerSpeed = 1200.0f;
+	Node3D twistPivot;
+	Node3D pitchPivot;
 
-    public override void _Ready()
-    {
-        Input.MouseMode = Input.MouseModeEnum.Captured;
-    }
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+		twistPivot = GetNode<Node3D>("TwistPivot");
+		pitchPivot = GetNode<Node3D>("TwistPivot/PitchPivot");
+	}
 
-    public override void _Process(double delta)
-    {
-        Vector3 input = Vector3.Zero;
-        input.X = Input.GetAxis("move_left", "move_right");
-        input.Z = Input.GetAxis("move_forward", "move_backward");
+	public override void _Process(double delta)
+	{
+		Vector3 input = Vector3.Zero;
+		input.X = Input.GetAxis("move_left", "move_right");
+		input.Z = Input.GetAxis("move_forward", "move_backward");
 
-        ApplyCentralForce(input * playerSpeed * (float)delta);
+		ApplyCentralForce(input * playerSpeed * (float)delta);
 
-        if (Input.IsActionJustPressed("cancel"))
-        {
-            Input.MouseMode = Input.MouseModeEnum.Visible;
-        }
-    }
+		if (Input.IsActionJustPressed("cancel"))
+		{
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+		}
 
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        if (@event is InputEventMouseMotion eventMouseMotion)
-        {
-            if (Input.MouseMode == Input.MouseModeEnum.Captured)
-            {
-                twistInput = -eventMouseMotion.Relative.X * mouseSensitivity;
-                pitchInput = -eventMouseMotion.Relative.Y * mouseSensitivity;
-                //38:01 in referenced YouTube video, continue from here!
-            }
-        }
-    }
+		twistPivot.RotateY(twistInput);
+		pitchPivot.RotateX(pitchInput);
+		var rotation = pitchPivot.Rotation;
+		rotation.X = (float)Mathf.Clamp(pitchPivot.Rotation.X, -0.5, 0.5);
+		pitchPivot.Rotation = rotation;
+
+		twistInput = 0.0f;
+		pitchInput = 0.0f;
+		//41:14 in reference youtube video
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion eventMouseMotion)
+		{
+			if (Input.MouseMode == Input.MouseModeEnum.Captured)
+			{
+				twistInput = -eventMouseMotion.Relative.X * mouseSensitivity;
+				pitchInput = -eventMouseMotion.Relative.Y * mouseSensitivity;
+			}
+		}
+	}
 }
