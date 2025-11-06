813b7006cf921bc81f8be2a17d5bcf629b04e9c3
Jerry <55096594+jerryf276@users.noreply.github.com>
2025-11-06 01:15:03 +0000

    commit: started mouse movement

--- honours-proj-feasibility-demo/Player.cs
+++ honours-proj-feasibility-demo/Player.cs
@@ -3,9 +3,15 @@ using System;
 
 public partial class Player : RigidBody3D
 {
+
+    float mouseSensitivity = 0.001f;
+    float twistInput = 0.0f;
+    float pitchInput = 0.0f;
+    float playerSpeed = 1200.0f;
+
     public override void _Ready()
     {
-        
+        Input.MouseMode = Input.MouseModeEnum.Captured;
     }
 
     public override void _Process(double delta)
@@ -14,6 +20,24 @@ public partial class Player : RigidBody3D
         input.X = Input.GetAxis("move_left", "move_right");
         input.Z = Input.GetAxis("move_forward", "move_backward");
 
-        ApplyCentralForce(input * 1200.0f * (float)delta);
+        ApplyCentralForce(input * playerSpeed * (float)delta);
+
+        if (Input.IsActionJustPressed("cancel"))
+        {
+            Input.MouseMode = Input.MouseModeEnum.Visible;
+        }
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion eventMouseMotion)
+        {
+            if (Input.MouseMode == Input.MouseModeEnum.Captured)
+            {
+                twistInput = -eventMouseMotion.Relative.X * mouseSensitivity;
+                pitchInput = -eventMouseMotion.Relative.Y * mouseSensitivity;
+                //38:01 in referenced YouTube video, continue from here!
+            }
+        }
     }
 }
