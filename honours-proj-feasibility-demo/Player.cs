ac35cde3e8a89f481de219c2ae2198bab350358e
Jerry <55096594+jerryf276@users.noreply.github.com>
2025-11-06 00:24:27 +0000

    added: player movement (partially)

--- honours-proj-feasibility-demo/Player.cs
+++ honours-proj-feasibility-demo/Player.cs
@@ -10,7 +10,8 @@ public partial class Player : RigidBody3D
 
     public override void _Process(double delta)
     {
-        var input = Input.GetActionStrength("ui_up");
-        ApplyCentralForce(Vector3.Forward * input * 1200.0 * delta);
+        var input = Input.GetActionStrength("move_forward");
+        ApplyCentralForce(new Vector3(Vector3.Forward.X, Vector3.Forward.Y, Vector3.Forward.Z * input * 1200.0f * (float)delta));
+        GD.Print(Position);
     }
 }
