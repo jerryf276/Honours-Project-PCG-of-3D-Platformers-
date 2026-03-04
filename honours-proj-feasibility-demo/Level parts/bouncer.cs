570b2e0cd04be2c4e5b8c60cc6a3d448c66216bc
Jerry <55096594+jerryf276@users.noreply.github.com>
2026-03-04 07:11:49 +0000

    added: bouncer

---
+++ honours-proj-feasibility-demo/Level parts/bouncer.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class bouncer : Area3D
+{
+    [Export] private AnimationPlayer;
+
+        public override void _Ready()
+        {
+      
+        }
+
+        public override void _Process(double delta)
+        {
+        
+        }
+
+        
+}
